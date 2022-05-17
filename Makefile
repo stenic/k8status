d2deaca182fffc89e34c33133529d3091c227f41
Dries De Peuter <dries@stenic.io>
2022-05-17 13:25:16 +0200

    feat: Add update time info

--- Makefile
+++ Makefile
@@ -9,6 +9,16 @@ run:
 	docker build -t k8status .
 	docker run -ti -p 8080:8080 -v ~/.kube:/home/nonroot/.kube k8status --namespace $(NS) $(ARGS)
 
+.PHONY: server
+server:
+	cd server \
+		&& go run main.go --namespace $(NS)
+
+.PHONY: ui
+ui:
+	cd ui \
+		&& npm start
+
 # go-get-tool will 'go get' any package $2 and install it to $1.
 PROJECT_DIR := $(shell dirname $(abspath $(lastword $(MAKEFILE_LIST))))
 define go-get-tool
--- ui/src/App.js
+++ ui/src/App.js
@@ -1,15 +1,19 @@
 import './App.css';
 import { useState, useEffect } from "react";
+import moment from 'moment'
+
 
 
 function App() {
   const [services, setServices] = useState([]);
+  const [updateInfo, setUpdateInfo] = useState("Loading");
 
   const fetchServices = () => {
     fetch('./status')
       .then(response => response.json())
       .then(response => {
         setServices(response)
+        setUpdateInfo("Last updated: "+moment(new Date()).format("LTS"))
       })
   }
 
@@ -22,24 +26,29 @@ function App() {
   
   return (
     <div className="App p-5">
-      <section class="hero">
-        <div class="hero-body">
-          <p class="title">
-            k8status
-          </p>
+      <div id="wrapper">
+        <section class="hero">
+          <div class="hero-body">
+            <p class="title">
+              k8status
+            </p>
+          </div>
+        </section>
+          <div class="tile is-ancestor is-flex-wrap-wrap">
+            {services.map((service, index) => {
+              return (
+                <div key={index} class="tile is-parent is-3">
+                  <article className={`tile is-child box notification ${service.ready ? 'is-primary' : 'is-danger'}`}>
+                    <p class="title">{service.name}</p>
+                  </article>
+                </div>
+              )
+            })}
+          </div>
         </div>
-      </section>
-        <div class="tile is-ancestor is-flex-wrap-wrap">
-          {services.map((service, index) => {
-            return (
-              <div key={index} class="tile is-parent is-3">
-                <article className={`tile is-child box notification ${service.ready ? 'is-primary' : 'is-danger'}`}>
-                  <p class="title">{service.name}</p>
-                </article>
-              </div>
-            )
-          })}
-        </div>
+        <p className='has-text-right has-text-weight-light	'>
+            {updateInfo}
+        </p>
     </div>
   );
 }
