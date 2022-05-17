b74ee63e0202a28304fc2d6a752131fe6c29350b
Dries De Peuter <dries@stenic.io>
2022-05-17 20:09:51 +0200

    fix: Sort ui

--- ui/src/App.js
+++ ui/src/App.js
@@ -12,6 +12,17 @@ function App() {
     fetch('./status')
       .then(response => response.json())
       .then(response => {
+        response.sort((a, b) => {
+          let fa = a.name.toLowerCase(),
+              fb = b.name.toLowerCase();
+          if (fa < fb) {
+              return -1;
+          }
+          if (fa > fb) {
+              return 1;
+          }
+          return 0;
+        })
         setServices(response)
         setUpdateInfo("Last updated: "+moment(new Date()).format("LTS"))
       })
